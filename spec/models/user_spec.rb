89a57c5008a57f95d7ab74495db0b7a96d1296d8
David Montesdeoca <info@davidmontesdeoca.net>
2016-10-22 17:10:57 +0200

    Generate SSH keys on user registration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :ssh_public_key }
+  it { is_expected.to validate_presence_of :ssh_private_key }
+
+  describe 'before validation' do
+    it 'does not assign any key when email is blank' do
+      user = build(:user, email: '')
+
+      expect { user.valid? }.to_not change { user.ssh_public_key }
+    end
+
+    it 'assigns SSH public key on create' do
+      user = build(:user)
+
+      expect { user.valid? }.to change { user.ssh_public_key }
+    end
+
+    it 'assigns SSH private key on create' do
+      user = build(:user)
+
+      expect { user.valid? }.to change { user.ssh_private_key }
+    end
+
+    it 'does not assign SSH public key on update' do
+      user = create(:user)
+
+      expect { user.save }.to_not change { user.ssh_public_key }
+    end
+
+    it 'does not assign SSH private key on update' do
+      user = create(:user)
+
+      expect { user.save }.to_not change { user.ssh_private_key }
+    end
+  end
 end
