c901f78f49d0d8ccefd8f3731cd910aaab900e41
Imobach González Sosa <igonzalezsosa@suse.com>
2016-10-24 00:07:25 +0100

    Start the deployment when the button is pushed

--- app/jobs/deployment_job.rb
+++ app/jobs/deployment_job.rb
@@ -1,10 +1,11 @@
 class DeploymentJob < ApplicationJob
   queue_as :default
 
-  def perform(deployment)
+  def perform(deployment_id)
+    deployment = Deployment.find(deployment_id)
     machine_deployment = deployment.next!
     return if machine_deployment.nil?
     Deployer.new(machine_deployment).deploy
-    DeploymentJob.perform_later(deployment)
+    DeploymentJob.perform_later(deployment.id)
   end
 end
