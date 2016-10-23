d01b084d57f26c5b5474a1a640e5f1aac7697f11
David Díaz <diazglezdavid@gmail.com>
2016-10-23 19:16:14 +0100

    Allow to know if machine is assigned

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,13 @@
 class Machine < ApplicationRecord
   belongs_to :user
+  has_many :assignments
 
   validates :user_id, :name, :ip, presence: true
+
+  # Check if machine is being used
+  #
+  # @return [Boolean] true if machine has assignments; false otherwise.
+  def assigned?
+    assignments.present?
+  end
 end
