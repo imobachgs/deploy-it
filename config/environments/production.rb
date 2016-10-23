1017cd18de990894864d7174ea0cfd4d150bd99f
David Montesdeoca <info@davidmontesdeoca.net>
2016-10-23 02:37:49 +0200

    Add SMTP configuration to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer
-  config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'], port: ENV['MAILER_PORT'] }
+  config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USER_NAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       ENV['SMTP_AUTHENTICATION'],
+    enable_starttls_auto: true
+  }
 end
