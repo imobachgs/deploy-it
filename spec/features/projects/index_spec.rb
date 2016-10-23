12fddbc3aa57fe22595526cff1187807065dec99
David Díaz <diazglezdavid@gmail.com>
2016-10-23 17:05:44 +0100

    Prevent to use project's creation form when no machines

--- spec/features/projects/index_spec.rb
+++ spec/features/projects/index_spec.rb
@@ -7,11 +7,34 @@ RSpec.feature 'Listing projects', type: :feature do
   background do
     visit root_path
     sign_in(user)
-    visit projects_path
   end
 
   scenario 'List all projects' do
+   visit projects_path
 
    expect(page).to have_content(project.name)
+  end
+
+  context 'User has machines' do
+    background do
+      create(:machine, user: user)
+      visit projects_path
+    end
+
+
+    scenario 'does not show new project link' do
+      expect(page).to have_link('Add new project')
+    end
+  end
+
+  context 'User has not machines' do
+    background do
+      visit projects_path
+    end
+
+
+    scenario 'does not show new project link' do
+      expect(page).to_not have_link('Add new project')
+    end
   end
 end
