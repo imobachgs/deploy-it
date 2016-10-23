54b0dd254c48ee443307975b5e79cc08b3641334
David Kang <gerheman@gmail.com>
2016-10-23 21:34:20 +0100

    fix typo

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :project do
     user
     name 'A Rails project'
-    repo_url 'http://exmaple.com'
+    repo_url 'http://example.com'
     desc 'The project description'
 
     association :kind, factory: :project_kind
