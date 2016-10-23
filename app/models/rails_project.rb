3db93a8c79929cfe24e9217acbbef271ea8044ef
David Kang <gerheman@gmail.com>
2016-10-23 05:24:04 +0100

    Add "rails_project" attributes in the form

--- app/models/rails_project.rb
+++ app/models/rails_project.rb
@@ -1,8 +1,18 @@
 class RailsProject < Project
-  validates :ruby_version, :adapter, :database,
-    :username, :password, :secret, presence: true, on: :update
+  before_validation :set_secure
+
+  validates :ruby_version, :database_adapter, :database_name,
+    :database_username, :database_password, :secret, presence: true, on: :update
 
   def self.roles
     RailsRole.all
+  end
+
+
+  private
+
+  def set_secure
+    self.secret = SecureRandom.hex(64)
+    self.db_admin_password = SecureRandom.hex(10)
   end
 end
