5b2dd8ce10d13546494fb8ded9c7b35f90de76a6
David Montesdeoca <info@davidmontesdeoca.net>
2016-10-24 02:01:40 +0200

    Fix error in job file

--- app/jobs/deployment_broadcast_job.rb
+++ app/jobs/deployment_broadcast_job.rb
@@ -2,6 +2,6 @@ class DeploymentBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(status, user:)
-    DeploymentChannel.broadcast_to user, status: status.
+    DeploymentChannel.broadcast_to user, status: status
   end
 end
